de99b31a76fa664ed508714b544493d7c103e6ce
jordan.boncz <jgbonc@gmail.com>
2024-04-17 18:25:26 -0400

    asd

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -1,51 +1,52 @@
 import ldapjs from 'ldapjs';
 
+async function bindAndSearchLdap(searchBase, filter, attributes) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const client = ldapjs.createClient({ url: 'ldap://ldap.forumsys.com:389' });
+            await new Promise((resolveClientBind, rejectClientBind) => {
+                client.bind('cn=read-only-admin,dc=example,dc=com', 'password', (err, res) => {
+                    if (err) {
+                        rejectClientBind(err);
+                        return;
+                    }
+                    console.log('LDAP bind successful');
+                    resolveClientBind(client);
+                });
+            });
+
+            const entries = await new Promise((resolveSearch, rejectSearch) => {
+                client.search(searchBase, { scope: 'sub', filter, attributes }, (err, res) => {
+                    if (err) {
+                        rejectSearch(err);
+                        return;
+                    }
+
+                    const entries = [];
+                    res.on('searchEntry', (entry) => {
+                        entries.push(entry.pojo);
+                    });
+                    res.on('end', () => {
+                        resolveSearch(entries);
+                    });
+                    res.on('error', (err) => {
+                        rejectSearch(err);
+                    });
+                });
+            });
+
+            resolve(entries);
+        }
+        catch (err) {
+            reject(err);
+        }
+        finally {
+
+        }
+    });
+}
+
 export async function POST(request) {
-
-    async function bindAndSearchLdap(searchBase, filter, attributes) {
-        return new Promise(async (resolve, reject) => {
-            try {
-                const client = ldapjs.createClient({ url: 'ldap://ldap.forumsys.com:389' });
-                await new Promise((resolveClientBind, rejectClientBind) => {
-                    client.bind('cn=read-only-admin,dc=example,dc=com', 'password', (err, res) => {
-                        if (err) {
-                            rejectClientBind(err);
-                            return;
-                        }
-                        console.log('LDAP bind successful');
-                        resolveClientBind(client);
-                    });
-                });
-
-                const entries = await new Promise((resolveSearch, rejectSearch) => {
-                    client.search(searchBase, { scope: 'sub', filter, attributes }, (err, res) => {
-                        if (err) {
-                            rejectSearch(err);
-                            return;
-                        }
-
-                        const entries = [];
-                        res.on('searchEntry', (entry) => {
-                            entries.push(entry.pojo);
-                        });
-                        res.on('end', () => {
-                            resolveSearch(entries);
-                        });
-                        res.on('error', (err) => {
-                            rejectSearch(err);
-                        });
-                    });
-                });
-
-                resolve(entries);
-            } catch (err) {
-                reject(err);
-            } finally {
-                // Consider adding logic to close the LDAP connection here
-            }
-        });
-    }
-
     let results = await bindAndSearchLdap(' dc=example,dc=com', '(uid=boyle)', ['uid'])
 
 
